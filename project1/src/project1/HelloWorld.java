baedddea4770024f63b7f3d53c02d55c295f83e1
19jy0231 <19jy0231@PC31.jynet2.jec.ac.jp>
2020-10-22 10:40:44 +0900

    add Javaを表示する行を追加

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("HelloWorld");
+		System.out.println("Java");
+		//System.out.println("ＨＴＭＬ");
 	}
 
 }
